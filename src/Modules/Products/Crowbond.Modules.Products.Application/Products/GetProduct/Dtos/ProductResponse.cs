6962996b717733d4c6754e86369ef46f075044b2
Reza Amini <r.amini@unleashed-digital.co.uk>
2024-06-02 16:37:26 +0100

    Adding Get Product by id service.

---
+++ src/Modules/Products/Crowbond.Modules.Products.Application/Products/GetProduct/Dtos/ProductResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace Crowbond.Modules.Products.Application.Products.GetProduct.Dtos;
+
+public sealed record ProductResponse(
+    Guid Id,
+    string Sku,
+    string Name,
+    Guid? Parent,
+    string FilterType,
+    string UnitOfMeasure,
+    Guid Category,
+    string InventoryType,
+    int? Barcode,
+    int? PackSize,
+    string HandlingNotes,
+    bool QiCheck,
+    string Notes,
+    int? ReorderLevel,
+    decimal? Height,
+    decimal? Width,
+    decimal? Length,
+    bool WeightInput,
+    bool Active
+    );
