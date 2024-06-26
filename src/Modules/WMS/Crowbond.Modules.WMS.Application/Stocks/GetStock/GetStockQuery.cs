08d1daf618d68208ef21b896706e0cbdd76ecb34
Reza Amini <reza.work@hotmail.com>
2024-06-26 10:56:19 +0100

    Add get stock by id endpoint.

---
+++ src/Modules/WMS/Crowbond.Modules.WMS.Application/Stocks/GetStock/GetStockQuery.cs
@@ -0,0 +1,5 @@
+﻿using Crowbond.Common.Application.Messaging;
+
+namespace Crowbond.Modules.WMS.Application.Stocks.GetStock;
+
+public sealed record GetStockQuery(Guid StockId) : IQuery<StockResponse>;
