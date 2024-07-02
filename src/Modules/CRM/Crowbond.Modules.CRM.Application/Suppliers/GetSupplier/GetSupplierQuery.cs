1ca7957895547be2726e49025473ecf72d352d9b
Marcel Larose <m.larose@unleashed-digital.co.uk>
2024-07-02 17:21:45 +0100

    Added Supplier Create, Update, Details

---
+++ src/Modules/CRM/Crowbond.Modules.CRM.Application/Suppliers/GetSupplier/GetSupplierQuery.cs
@@ -0,0 +1,6 @@
+﻿using Crowbond.Common.Application.Messaging;
+
+namespace Crowbond.Modules.CRM.Application.Suppliers.GetSupplier;
+
+public sealed record GetSupplierQuery(Guid SupplierId) : IQuery<SupplierResponse>;
+
---
+++ src/Modules/CRM/Crowbond.Modules.CRM.Application/Suppliers/UpdateSupplier/UpdateSupplierCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace Crowbond.Modules.CRM.Application.Suppliers.UpdateSupplier;
+
+internal sealed class UpdateSupplierCommandValidator : AbstractValidator<UpdateSupplierCommand>
+{
+    public UpdateSupplierCommandValidator()
+    {
+        RuleFor(s => s.Supplier.SupplierName).NotEmpty();
+        RuleFor(s => s.Supplier.AddressLine1).NotEmpty();
+        RuleFor(s => s.Supplier.AddressLine2).NotEmpty();
+        RuleFor(s => s.Supplier.AddressTownCity).NotEmpty();
+        RuleFor(s => s.Supplier.AddressPostalCode).NotEmpty();
+        RuleFor(s => s.Supplier.SupplierEmail).NotEmpty();
+        RuleFor(s => s.Supplier.SupplierPhone).NotEmpty();
+        RuleFor(s => s.Supplier.SupplierContact).NotEmpty();
+        RuleFor(s => s.Supplier.SupplierNotes).MaximumLength(500);
+    }
+}
