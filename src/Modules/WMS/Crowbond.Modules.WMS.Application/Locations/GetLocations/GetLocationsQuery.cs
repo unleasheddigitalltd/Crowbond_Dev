d840d1653a052ecc32232cd4ac9959b6406128db
Reza Amini <reza.work@hotmail.com>
2024-07-04 11:51:36 +0100

    Add get locations endpoint.

---
+++ src/Modules/WMS/Crowbond.Modules.WMS.Application/Locations/GetLocations/GetLocationsQuery.cs
@@ -0,0 +1,4 @@
+﻿using Crowbond.Common.Application.Messaging;
+
+namespace Crowbond.Modules.WMS.Application.Locations.GetLocations;
+public sealed record GetLocationsQuery(string LocationType) : IQuery<IReadOnlyCollection<LocationResponse>>;
---
+++ src/Modules/WMS/Crowbond.Modules.WMS.Presentation/Locations/GetLocations.cs
@@ -0,0 +1,23 @@
+﻿using Crowbond.Common.Domain;
+using Crowbond.Common.Presentation.Endpoints;
+using Crowbond.Common.Presentation.Results;
+using Crowbond.Modules.WMS.Application.Locations.GetLocations;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Crowbond.Modules.WMS.Presentation.Locations;
+
+internal sealed class GetLocations : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("locations/{locationType}", async (string locationType, ISender sender) =>
+        {
+            Result<IReadOnlyCollection<LocationResponse>> result = await sender.Send(new GetLocationsQuery(locationType));
+            return result.Match(Results.Ok, ApiResults.Problem);
+        }).RequireAuthorization(Permissions.GetStocks)
+        .WithTags(Tags.Stocks);
+    }
+}
