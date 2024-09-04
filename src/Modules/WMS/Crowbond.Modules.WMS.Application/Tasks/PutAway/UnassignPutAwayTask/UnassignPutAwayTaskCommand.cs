fb627b5e462ecf27cf9f2b5913d8237b1def0614
Reza Amini <r.amini@unleashed-digital.co.uk>
2024-09-04 10:21:41 +0100

    Adding the unassin putaway task endpoint.

---
+++ src/Modules/WMS/Crowbond.Modules.WMS.Application/Tasks/PutAway/UnassignPutAwayTask/UnassignPutAwayTaskCommand.cs
@@ -0,0 +1,5 @@
+﻿using Crowbond.Common.Application.Messaging;
+
+namespace Crowbond.Modules.WMS.Application.Tasks.PutAway.UnassignPutAwayTask;
+
+public sealed record UnassignPutAwayTaskCommand(Guid UserId, Guid TaskHeaderId) : ICommand;
---
+++ src/Modules/WMS/Crowbond.Modules.WMS.Application/Tasks/PutAway/UnassignPutAwayTask/UnassignPutAwayTaskCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Crowbond.Modules.WMS.Application.Tasks.PutAway.UnassignPutAwayTask;
+
+internal sealed class UnassignPutAwayTaskCommandValidator : AbstractValidator<UnassignPutAwayTaskCommand>
+{
+    public UnassignPutAwayTaskCommandValidator()
+    {
+        RuleFor(t => t.TaskHeaderId).NotEmpty();
+    }
+}
