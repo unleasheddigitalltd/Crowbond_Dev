bc920ab6733d46d59d32c900114a677564fd47e8
Reza Amini <r.amini@unleashed-digital.co.uk>
2024-10-18 12:15:58 +0100

    Adding the get picking task assignment line details.

---
+++ src/Modules/WMS/Crowbond.Modules.WMS.Application/Tasks/Picking/GetMyPickingTaskAssignmentLineDatails/GetMyPickingTaskAssignmentLineDatailsQuery.cs
@@ -0,0 +1,5 @@
+﻿using Crowbond.Common.Application.Messaging;
+
+namespace Crowbond.Modules.WMS.Application.Tasks.Picking.GetMyPickingTaskAssignmentLineDatails;
+
+public sealed record GetMyPickingTaskAssignmentLineDatailsQuery(Guid WarehouseOperatorId, Guid TaskAssignmentLineId) : IQuery<TaskAssignmentLineDetailsResponse>;
---
+++ src/Modules/WMS/Crowbond.Modules.WMS.Application/Tasks/Picking/GetMyPickingTaskAssignmentLineDatails/TaskAssignmentLineDetailsResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Crowbond.Modules.WMS.Application.Tasks.Picking.GetMyPickingTaskAssignmentLineDatails;
+
+public sealed record TaskAssignmentLineDetailsResponse(
+    Guid Id,
+    Guid ProductId,
+    string ProductName,
+    string ProductSku,
+    decimal RequestedQty,
+    decimal CompletedQty);
