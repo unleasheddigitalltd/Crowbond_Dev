4d1d0a2c34e53c1d926cef7651b3f38f5f70b234
Reza Amini <r.amini@unleashed-digital.co.uk>
2025-01-20 15:32:46 +0000

    Adding get stocks by location

---
+++ src/Modules/WMS/Crowbond.Modules.WMS.Presentation/Stocks/GetStocksByLocation.cs
@@ -0,0 +1,25 @@
+﻿using Crowbond.Common.Domain;
+using Crowbond.Common.Presentation.Endpoints;
+using Crowbond.Common.Presentation.Results;
+using Crowbond.Modules.WMS.Application.Stocks.GetStocksByLocation;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Crowbond.Modules.WMS.Presentation.Stocks;
+
+internal sealed class GetStocksByLocation : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("stocks/locations/{id}", async (Guid id, ISender sender) =>
+        {
+            Result<IReadOnlyCollection<StockResponse>> result = await sender.Send(new GetStocksByLocationQuery(id));
+
+            return result.Match(Results.Ok, ApiResults.Problem);
+        })
+            .RequireAuthorization(Permissions.GetStocks)
+            .WithTags(Tags.Stocks);
+    }
+}
