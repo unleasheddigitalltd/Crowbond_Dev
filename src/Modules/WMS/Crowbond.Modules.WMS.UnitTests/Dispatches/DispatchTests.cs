c64a1f31cdf2896ef0b1740a71863bdaccd2f6ba
Reza Amini <r.amini@unleashed-digital.co.uk>
2025-01-24 16:30:45 +0000

    Adding dispatch and location unit tests

--- src/Modules/WMS/Crowbond.Modules.WMS.UnitTests/Dispatches/DispatchTests.cs
+++ src/Modules/WMS/Crowbond.Modules.WMS.UnitTests/Dispatches/DispatchTests.cs
@@ -28,4 +28,36 @@ public class DispatchTests : BaseTest
         result.Value.Status.Should().Be(DispatchStatus.NotStarted);
         result.Value.Lines.Should().BeEmpty();
     }
+
+    [Fact]
+    public void AddLine_ShouldReturnNewDispatchLine_WhenInputIsValid()
+    {
+        // Arrange
+        string dispatchNo = Faker.Random.AlphaNumeric(10);
+        var routeTripId = Guid.NewGuid();
+        DateOnly routeTripDate = Faker.Date.RecentDateOnly();
+        string routeName = Faker.Address.City();
+        Result<DispatchHeader> header = DispatchHeader.Create(dispatchNo, routeTripId, routeTripDate, routeName);
+        var orderId = Guid.NewGuid();
+        string orderNo = Faker.Random.AlphaNumeric(10);
+        string customerBusinessName = Faker.Company.Locale;
+        var orderLineId = Guid.NewGuid();
+        var productId = Guid.NewGuid();
+        decimal orderQty = Faker.Random.Decimal(0);
+
+        // Act
+        Result<DispatchLine> result = header.Value.AddLine(orderId, orderNo, customerBusinessName, orderLineId, productId, orderQty);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.OrderId.Should().Be(orderId);
+        result.Value.OrderNo.Should().Be(orderNo);
+        result.Value.CustomerBusinessName.Should().Be(customerBusinessName);
+        result.Value.OrderLineId.Should().Be(orderLineId);
+        result.Value.ProductId.Should().Be(productId);
+        result.Value.OrderedQty.Should().Be(orderQty);
+        result.Value.PickedQty.Should().Be(0);
+        result.Value.IsPicked.Should().Be(false);
+        header.Value.Lines.First().Should().Be(result.Value);
+    }
 }
--- src/Modules/WMS/Crowbond.Modules.WMS.UnitTests/Locations/LocationTests.cs
+++ src/Modules/WMS/Crowbond.Modules.WMS.UnitTests/Locations/LocationTests.cs
@@ -29,4 +29,86 @@ public class LocationTests : BaseTest
         result.Value.LocationLayer.Should().Be(locationLayer);
         result.Value.Status.Should().Be(LocationStatus.Active);
     }
+
+    [Fact]
+    public void Create_ShouldReturnFailer_WhenLayerIsNotLocationAndTypeIsNotNull()
+    {
+        // Arrange
+        var parentId = Guid.NewGuid();
+        string name = Faker.Random.AlphaNumeric(10);
+        string scanCode = Faker.Random.AlphaNumeric(10);
+        LocationType? locationType = Enum.GetValues(typeof(LocationType)).Cast<LocationType>().FirstOrDefault();
+        LocationLayer nonLocationLayer = Enum.GetValues(typeof(LocationLayer))
+                               .Cast<LocationLayer>()
+                               .FirstOrDefault(layer => layer != LocationLayer.Location);
+
+        // Act
+        Result<Location> result = Location.Create(parentId, name, scanCode, locationType, nonLocationLayer);
+
+        // Assert
+        result.Error.Should().Be(LocationErrors.InvalidLocationTypeAssignment);
+    }
+
+    [Fact]
+    public void Create_ShouldReturnFailer_WhenParentIsNullAndLayerIsAreaOrLocation()
+    {
+        // Arrange
+        Guid? parentId = null;
+        string name = Faker.Random.AlphaNumeric(10);
+        string scanCode = Faker.Random.AlphaNumeric(10);
+        
+        LocationLayer[] locationLayers = { LocationLayer.Area, LocationLayer.Location };
+
+        foreach (LocationLayer layer in locationLayers)
+        {
+            LocationType? locationType = layer == LocationLayer.Location ? LocationType.Putaway : null;
+            // Act
+            Result<Location> result = Location.Create(parentId, name, scanCode, locationType, layer);
+
+            // Assert
+            result.Error.Should().Be(LocationErrors.NeedParent);
+        }
+    }
+
+    [Fact]
+    public void Create_ShouldReturnFailer_WhenLayerIsSiteAndParentIsNotNull()
+    {
+        // Arrange
+        var parentId = Guid.NewGuid();
+        string name = Faker.Random.AlphaNumeric(10);
+        string scanCode = Faker.Random.AlphaNumeric(10);
+        LocationType? locationType = null;
+        LocationLayer locationLayer = LocationLayer.Site;
+
+        // Act
+        Result<Location> result = Location.Create(parentId, name, scanCode, locationType, locationLayer);
+
+        // Assert
+        result.Error.Should().Be(LocationErrors.CanNotHaveParent);
+    }
+
+    [Fact]
+    public void Update_ShouldReturnSucces_WhenInputIsValid()
+    {
+        // Arrange
+        var parentId = Guid.NewGuid();
+        string name = Faker.Random.AlphaNumeric(10);
+        string scanCode = Faker.Random.AlphaNumeric(10);
+        LocationType locationType = LocationType.Putaway;
+        LocationLayer locationLayer = LocationLayer.Location;
+
+        Result<Location> location = Location.Create(parentId, name, scanCode, locationType, locationLayer);
+
+        var newParentId = Guid.NewGuid();
+        string newName = Faker.Random.AlphaNumeric(10);
+        string newScanCode = Faker.Random.AlphaNumeric(10);
+        LocationType? newLocationType = null;
+        LocationLayer newLocationLayer = LocationLayer.Area;
+
+        // Act
+        Result result = location.Value.Update(newParentId, newName, newScanCode, newLocationType, newLocationLayer);
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+    }
 }
