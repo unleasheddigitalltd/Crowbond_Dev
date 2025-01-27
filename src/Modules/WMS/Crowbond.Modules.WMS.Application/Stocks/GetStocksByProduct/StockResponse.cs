c5573a293536003ddd1153f78d43aae6a85f16f1
Reza Amini <r.amini@unleashed-digital.co.uk>
2025-01-27 11:00:55 +0000

    Adding get stocks by product id.

---
+++ src/Modules/WMS/Crowbond.Modules.WMS.Application/Stocks/GetStocksByProduct/StockResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Crowbond.Modules.WMS.Application.Stocks.GetStocksByProduct;
+
+public sealed record StockResponse(
+    Guid Id,
+    string Sku,
+    string Name,
+    string CategoryName,
+    string Batch,
+    string UnitOfMeasureName,
+    decimal InStock,
+    string Location,
+    int DaysInStock,
+    bool IsActive,
+    string Status);
