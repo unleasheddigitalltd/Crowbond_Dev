675d13b25bd4db02feb3d27d5166648ab93d7e96
james <jphgardner02@gmail.com>
2025-04-22 09:41:01 +0100

    Get Supplier Products with Stock

--- src/Modules/CRM/Crowbond.Modules.CRM.Presentation/SupplierProducts/GetSupplierProducts.cs
+++ src/Modules/CRM/Crowbond.Modules.CRM.Presentation/SupplierProducts/GetSupplierProducts.cs
@@ -1,7 +1,10 @@
 ﻿using Crowbond.Common.Domain;
 using Crowbond.Common.Presentation.Endpoints;
 using Crowbond.Common.Presentation.Results;
+using Crowbond.Modules.CRM.Application.CustomerProducts.GetCustomerProduct;
 using Crowbond.Modules.CRM.Application.SupplierProducts.GetSupplierProducts;
+using Crowbond.Modules.CRM.Application.SupplierProducts.GetSupplierProductsWithStock;
+using Crowbond.Modules.WMS.Application.Products.GetProducts;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
@@ -14,12 +17,21 @@ internal sealed class GetSupplierProducts : IEndpoint
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
         app.MapGet("suppliers/{id}/products", async (Guid id, ISender sender) =>
-        {
-            Result<IReadOnlyCollection<SupplierProductResponse>> result = await sender.Send(new GetSupplierProductsQuery(id));
+            {
+                Result<IReadOnlyCollection<SupplierProductResponse>> result =
+                    await sender.Send(new GetSupplierProductsQuery(id));
 
-            return result.Match(Results.Ok, ApiResults.Problem);
-        })
-            .RequireAuthorization(Permissions.GetSuppliers)
+                return result.Match(Results.Ok, ApiResults.Problem);
+            }).RequireAuthorization(Permissions.GetSuppliers)
+            .WithTags(Tags.Suppliers);
+        
+        app.MapGet("suppliers/{id}/products/with-stock", async (Guid id, ISender sender) =>
+            {
+                Result<IReadOnlyCollection<ProductWithStockResponse>> result =
+                    await sender.Send(new GetSupplierProductsWithStockQuery(id));
+
+                return result.Match(Results.Ok, ApiResults.Problem);
+            }).RequireAuthorization(Permissions.GetSuppliers)
             .WithTags(Tags.Suppliers);
     }
 }
