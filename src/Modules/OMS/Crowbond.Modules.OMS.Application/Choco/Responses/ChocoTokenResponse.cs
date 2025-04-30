6f2361fca737cf0810451949c5cd2194706eabb0
james <jphgardner02@gmail.com>
2025-04-30 17:18:49 +0100

    Add logging for Choco access token and update ChocoTokenResponse to include JsonProperty attribute

--- src/Modules/OMS/Crowbond.Modules.OMS.Application/Choco/Responses/ChocoTokenResponse.cs
+++ src/Modules/OMS/Crowbond.Modules.OMS.Application/Choco/Responses/ChocoTokenResponse.cs
@@ -1,6 +1,9 @@
+using Newtonsoft.Json;
+
 namespace Crowbond.Modules.OMS.Application.Choco.Responses;
 
 public class ChocoTokenResponse
 {
+    [JsonProperty("access_token")]
     public string AccessToken { get; set; }
 }
